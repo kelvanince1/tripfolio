77746027b1e8b72c307dca268ea1d38defacc289
Kelvan Ince <incekelvan@gmail.com>
2016-11-14 12:03:52 -0600

    Reworked Yelp API into backend file. This commit shouldn't change your file much.

--- src/components/NewTripModal.js
+++ src/components/NewTripModal.js
@@ -1,17 +1,28 @@
 // Modules
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import yelp from '../utils/yelp';
 
 // Components
+const yelpCallback = (error, response, body) => {
+  console.log('yelpCallback');
+  console.log('error', error);
+  console.log('response');
+}
+
+yelp({}, yelpCallback)
 
 // Styles and images
 
 class NewTripModal extends Component {
+  _handleClick() {
+    console.log(yelp);
+  }
   render() {
     return(
       <main>
         <form>
-          <h4>Where do you want to go?</h4>
+          <h4 onClick={this._handleClick.bind(this)}>Where do you want to go?</h4>
           <input type="text" />
           <Link to="/planner">Get Started!</Link>
         </form>

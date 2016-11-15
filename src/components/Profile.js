2ea552e285833d55e652815c40b62cc5ee8e8840
Kelvan Ince <incekelvan@gmail.com>
2016-11-15 17:02:46 -0600

    Created search bar for users with no prior trips

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,19 +5,33 @@ import _ from 'lodash';
 
 // Components
 import LogoutButton from './LogoutButton';
+import NewTripModal from './NewTripModal';
 // Styles and images
 
 class Profile extends Component {
+    constructor(props) {
+      super(props);
 
-    // if (_.isEmpty(this.props.user)) {
-    //   return
-    //   <form>
-    //     <input type="submit" value="Where would you like to visit" />
-    //   </form>
-    // }
-    // else {
-    //   return('.container')
-    // }
+      this._renderTrips = this._renderTrips.bind(this);
+    }
+
+    _renderTrips() {
+      if (_.isEmpty(this.props.trips)) {
+        return <NewTripModal />
+      }
+      else {
+        return(
+        <ul>
+          {_.map(this.props.trips, (trip, tripId) => {
+            let destination = _.capitalize(trip.destination);
+            return <li key={tripId} data-tripId={tripId}><Link to={`/planner/${tripId}`}>My trip to {destination}</Link></li>
+          })}
+        </ul>
+      )
+      }
+    }
+
+
     render() {
       return(
           <main className="container">
@@ -36,12 +50,7 @@ class Profile extends Component {
               <div className="trips">
                 <div id="myTrips">
                   <h2>My Trips</h2>
-                    <ul>
-                      {_.map(this.props.trips, (trip, tripId) => {
-                        let destination = _.capitalize(trip.destination);
-                        return <li key={tripId} data-tripId={tripId}><Link to={`/planner/${tripId}`}>My trip to {destination}</Link></li>
-                      })}
-                    </ul>
+                  {this._renderTrips()}
                 </div>
               </div>
               </div>

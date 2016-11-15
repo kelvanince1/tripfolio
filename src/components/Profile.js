ae5e3707aa821e6d342e70e2877c3c02096bf86d
Kelvan Ince <incekelvan@gmail.com>
2016-11-15 16:47:31 -0600

    Minor alterations to profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,51 +1,53 @@
 // Modules
 import React, {Component} from 'react';
 import { Link } from 'react-router';
+import _ from 'lodash';
 
 // Components
 import LogoutButton from './LogoutButton';
 // Styles and images
 
 class Profile extends Component {
-  render() {
-    return(
-      <main className="container">
-        <Link to="/newTrip" id="trips-button" className="btn btn-default">My Trips</Link>
-        <div id="logout">
-          <LogoutButton firebase={this.props.route.firebase}>Logout</LogoutButton>
-        </div>
-        <div className="row">
-          <div className="profile">
-            <div id="profileInfo">
-              <img src={this.props.user.providerData[0].photoURL} alt="Profile Picture" id="profPic" />
-            </div>
-            <h2 id="where">Where would you like to go?</h2>
-            <h2 id="linkTrip"><Link to="/newTrip" id="newTripId">New Trip</Link></h2>
-          </div>
-          <div className="trips">
-            <div id="myTrips">
-              <h2>My Trips</h2>
-              <ul>
-                <li><Link to="/planner">Name of destination</Link></li>
-                <li><Link to="/planner">My trip to Madrid</Link></li>
-              </ul>
-            </div>
-            <div className="trips">
-              <div id="myTrips">
-                <h2>My Trips</h2>
-                <ul>
-                  {_.map(this.props.trips, (trip, tripId) => {
-                    let destination = _.capitalize(trip.destination);
 
-                    return <li key={tripId} data-tripId={tripId}><Link to={`/planner/${tripId}`}>My trip to {destination}</Link></li>
-                  })}
-                </ul>
+    // if (_.isEmpty(this.props.user)) {
+    //   return
+    //   <form>
+    //     <input type="submit" value="Where would you like to visit" />
+    //   </form>
+    // }
+    // else {
+    //   return('.container')
+    // }
+    render() {
+      return(
+          <main className="container">
+            <Link to="/newTrip" id="trips-button" className="btn btn-default">My Trips</Link>
+            <div id="logout">
+              <LogoutButton firebase={this.props.route.firebase}>Logout</LogoutButton>
+            </div>
+            <div className="row">
+              <div className="profile">
+                <div id="profileInfo">
+                  <img src={this.props.user.providerData[0].photoURL} alt="Profile Picture" id="profPic" />
+                </div>
+                <h2 id="where">Where would you like to go?</h2>
+                <h2 id="linkTrip"><Link to="/newTrip" id="newTripId">New Trip</Link></h2>
               </div>
-            </div> {/* Close col-md-6 div */}
-          </div> {/* Close row div */}
-        </main>
-    );
-  }
+              <div className="trips">
+                <div id="myTrips">
+                  <h2>My Trips</h2>
+                    <ul>
+                      {_.map(this.props.trips, (trip, tripId) => {
+                        let destination = _.capitalize(trip.destination);
+                        return <li key={tripId} data-tripId={tripId}><Link to={`/planner/${tripId}`}>My trip to {destination}</Link></li>
+                      })}
+                    </ul>
+                </div>
+              </div>
+              </div>
+            </main>
+        );
+    }
 }
 
 export default Profile;

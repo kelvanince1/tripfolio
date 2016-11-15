862f4d45e8abf78c2372da2b7a315a1e31fb0555
Clark Sanford <clrksanford@gmail.com>
2016-11-15 16:00:29 -0600

    Load in saved tiles and update as users save new places

--- src/components/SuggestionBox.js
+++ src/components/SuggestionBox.js
@@ -24,6 +24,7 @@ class SuggestionBox extends Component {
       <div className="container">
         <Link to="/profile" id="profile-button" className="btn btn-default">See my profile</Link>
         <div className="row" id="suggestionContainer">
+          <h3>Suggestions</h3>
           {_.map(this.props.results, (business, index) => {
             let image = business["image_url"];
             let name = business.name;
